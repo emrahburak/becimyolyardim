ead39838fbb1d4de7f3295ae30be7701f463dff0
emrahburak <gemrahburak@gmail.com>
2025-11-13 00:49:48 +0300

    UPDATED --Hero.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,27 +6,56 @@ export default function Hero() {
     <div className="relative w-full h-[720px] flex">
       {/* Sol taraf: mekanik çarklar */}
       <div
-        className="w-1/2 h-full relative bg-cover bg-center opacity-25"
+        className="w-1/2 h-full relative bg-cover bg-center"
         style={{
-          backgroundImage: `url(${MechanicalGears})`,
+          backgroundImage: `linear-gradient(rgba(255,255,255,0.9), rgba(255,255,255,0.9)), url(${MechanicalGears})`,
           backgroundPosition: 'right center',
           backgroundSize: 'cover',
         }}
       >
-        <div className="absolute top-0 left-0 w-full h-full bg-gray-200 opacity-10 "></div>
+        <div className="absolute top-0 left-0 bottom-0 w-full h-full bg-transparent z-10 flex items-center justify-center">
+          <div className="text-regal-black font-semibold">
+            Ac, eu vestibulum penatibus arcu tempor ipsum id nunc ligula.
+          </div>
+        </div>
       </div>
 
+
       {/* Sağ taraf: işletme fotoğrafı, clip-path ile yamuk */}
-      <div
-        className="hidden lg:block w-1/2 h-full bg-cover bg-center "
-        style={{
-          backgroundImage: `url(${BusinessPhoto})`,
-          clipPath: 'polygon(25% 0, 100% 0%, 100% 100%, 0 100%)'
-        }}
-      >
-        {/* İstersen overlay ekleyebilirsin */}
-        <div className="absolute top-0 left-0 w-full h-full bg-gray-200 opacity-10  "></div>
+      <div className="relative hidden lg:block w-1/2 h-full overflow-hidden">
+        {/* Fotoğraf */}
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{
+            backgroundImage: `url(${BusinessPhoto})`,
+            clipPath: 'polygon(25% 0, 100% 0%, 100% 100%, 0 100%)',
+          }}
+        >
+          {/* Kırmızı eğik overlay */}
+          <div
+            className="absolute bottom-0 left-0  w-1/2 h-1/2 bg-regal-red opacity-40"
+            style={{
+              clipPath: 'polygon(0 26%, 18% 0, 75% 100%, 36% 100%)',
+
+            }}
+          ></div>
+
+        </div>
+
+        {/* Kırmızı kenar efekti */}
+        <div
+          className="absolute top-0 left-0 h-full pointer-events-none"
+          style={{
+            width: '25%',
+            background:
+              'linear-gradient(to bottom right, transparent 49%, #ff0000 50%, transparent 51%)',
+          }}
+        ></div>
       </div>
     </div>
   )
 }
+
+
+
+
